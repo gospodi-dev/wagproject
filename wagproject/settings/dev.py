32286962136dda2ee393f06d1f3617b4e2c81261
Gospodi <35374510+gospodi-dev@users.noreply.github.com>
2023-11-26 23:32:08 +0300

    1

---
+++ wagproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-xo=jo3uq(bbn-&x&3#)@+r=^ta-__cqao&j7lu_v8npooh_l+)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
